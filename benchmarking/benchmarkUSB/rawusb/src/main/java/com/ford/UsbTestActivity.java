6e83fb4d26240c669ff0652cd952981f43c38286
Christopher Peplin <cpeplin@ford.com>
2011-10-18 14:54:46 -0400

    Auto-detect when USB device is plugged in and unplugged.

--- benchmarking/benchmarkUSB/rawusb/src/main/java/com/ford/UsbTestActivity.java
+++ benchmarking/benchmarkUSB/rawusb/src/main/java/com/ford/UsbTestActivity.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+
 import android.hardware.usb.UsbManager;
 import android.hardware.usb.UsbDevice;
 
@@ -12,19 +16,39 @@ import android.app.ListActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 import android.widget.ArrayAdapter;
 
-public class HelloAndroidActivity extends ListActivity {
+public class UsbTestActivity extends ListActivity {
 
     private static String TAG = "rawusb";
+    private Handler mHandler;
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "Refreshing USB devices as something was " +
+                    "plugged/unplugged");
+            UsbTestActivity.this.refreshDeviceList();
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 		Log.i(TAG, "onCreate");
 
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.hardware.usb.action.USB_DEVICE_ATTACHED");
+        filter.addAction("android.hardware.usb.action.USB_DEVICE_DETACHED");
+        registerReceiver(mBroadcastReceiver, filter);
+
+        mHandler = new Handler();
+        refreshDeviceList();
+    }
+
+    public void refreshDeviceList() {
         UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
         Map<String, UsbDevice> devices = manager.getDeviceList();
 
@@ -33,7 +57,7 @@ public class HelloAndroidActivity extends ListActivity {
         if(devices.size() > 0) {
             deviceNames = new ArrayList<String>(devices.keySet());
             for(String deviceName : deviceNames) {
-                Log.i(TAG, "Availble device: " + deviceName);
+                Log.i(TAG, "Available USB device: " + deviceName);
             }
 
         }

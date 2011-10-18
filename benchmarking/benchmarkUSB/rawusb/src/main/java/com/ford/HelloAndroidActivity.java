4a670a854dbfe739d1986b77b2d8af0e42fb7c3b
Christopher Peplin <cpeplin@ford.com>
2011-10-18 11:38:18 -0400

    Switch to Android API 3.1 for USB Host support.

--- benchmarking/benchmarkUSB/rawusb/src/main/java/com/ford/HelloAndroidActivity.java
+++ benchmarking/benchmarkUSB/rawusb/src/main/java/com/ford/HelloAndroidActivity.java
@@ -1,25 +1,31 @@
 package com.ford;
 
+import java.util.Map;
+
+import android.hardware.usb.UsbManager;
+import android.hardware.usb.UsbDevice;
+
 import android.app.Activity;
+import android.app.ListActivity;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
-public class HelloAndroidActivity extends Activity {
+import android.widget.ArrayAdapter;
+
+public class HelloAndroidActivity extends ListActivity {
 
     private static String TAG = "rawusb";
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 		Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
+
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        Map<String, UsbDevice> devices = manager.getDeviceList();
+        setListAdapter(new ArrayAdapter<String>(this,
+                    R.layout.simple_list_item_1, devices.keySet()));
     }
-
 }
-

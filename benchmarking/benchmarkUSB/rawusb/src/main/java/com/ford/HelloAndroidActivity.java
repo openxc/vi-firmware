4c46dd90f848fef2bb7f17cf3457104f86df44d7
Christopher Peplin <cpeplin@ford.com>
2011-10-18 13:55:11 -0400

    Load a list of USB devices and display in a list.

--- benchmarking/benchmarkUSB/rawusb/src/main/java/com/ford/HelloAndroidActivity.java
+++ benchmarking/benchmarkUSB/rawusb/src/main/java/com/ford/HelloAndroidActivity.java
@@ -1,5 +1,7 @@
 package com.ford;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import android.hardware.usb.UsbManager;
@@ -25,7 +27,18 @@ public class HelloAndroidActivity extends ListActivity {
 
         UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
         Map<String, UsbDevice> devices = manager.getDeviceList();
+
+        List<String> deviceNames = new ArrayList<String>();
+        Log.i(TAG, "There are " + devices.size() + " USB devices available");
+        if(devices.size() > 0) {
+            deviceNames = new ArrayList<String>(devices.keySet());
+            for(String deviceName : deviceNames) {
+                Log.i(TAG, "Availble device: " + deviceName);
+            }
+
+        }
         setListAdapter(new ArrayAdapter<String>(this,
-                    R.layout.simple_list_item_1, devices.keySet()));
+                    android.R.layout.simple_list_item_1,
+                    deviceNames));
     }
 }

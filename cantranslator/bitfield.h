1fc1a9b0fa4d2c19dff0b47d36ed8c4927af1a05
Christopher Peplin <chris.peplin@rhubarbtech.com>
2011-10-20 21:44:59 -0400

    Make get_bit_fields function more generic - accepts plain data, not CAN message.

--- cantranslator/bitfield.h
+++ cantranslator/bitfield.h
@@ -2,9 +2,7 @@
 #define _BITFIELD_H_
 
 #include "WProgram.h"
-#include "chipKITCAN.h"
 
-unsigned long get_bit_field(CAN::RxMessageBuffer* message, int startPos,
-        int numBits);
+unsigned long get_bit_field(uint8_t* data, int startPos, int numBits);
 
 #endif // _BITFIELD_H_

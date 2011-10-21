4b33abb2ff9a2515357ce5ad989245d3bb046f44
Christopher Peplin <chris.peplin@rhubarbtech.com>
2011-10-21 15:55:08 -0400

    Add CanSignal struct and a generic handler function for it.

--- cantranslator/canutil.h
+++ cantranslator/canutil.h
@@ -4,8 +4,6 @@
 #include "WProgram.h"
 #include "chipKITCAN.h"
 
-void configure_hs_filters(CAN *canMod);
-
 struct CanFilterMask {
     int number;
     int value;
@@ -17,5 +15,20 @@ struct CanFilter {
     int channel;
     int maskNumber;
 };
+
+struct CanSignal {
+    int id;
+    char* name;
+    char* humanName;
+    char* genericName;
+    int bitPosition;
+    int bitSize;
+    bool transform;
+    float factor;
+    float offset;
+};
+
+void configure_hs_filters(CAN *canMod);
+void decode_can_message(uint8_t* data, CanSignal signal);
 
 #endif // _CANUTIL_U

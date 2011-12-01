c38299d2d204e14701f4bbb3cad51fd01bd0f736
Christopher Peplin <cpeplin@ford.com>
2011-12-01 15:01:15 -0500

    Copy message to a new buffer after waiting for USB write to finish.
    I don't think this really adds anything over the other method of just blocking
    until it's really truly written. I'm going to try it again with a different
    approach.

--- cantranslator/usbutil.cpp
+++ cantranslator/usbutil.cpp
@@ -3,22 +3,30 @@
 USBDevice USB_DEVICE(usbCallback);
 USB_HANDLE USB_INPUT_HANDLE = 0;
 
+char SEND_BUFFER[SEND_BUFFER_SIZE];
+
 void sendMessage(uint8_t* message, int messageSize) {
-    int nextByteIndex = 0;
 #ifdef DEBUG
     Serial.print("sending message: ");
     Serial.println((char*)message);
 #endif
 
+    // Make sure the USB write is 100% complete before messing with this buffer
+    // after we copy the message into it - the Microchip library doesn't copy
+    // the data to its own internal buffer. See #171 for background on this
+    // issue.
+    while(USB_DEVICE.HandleBusy(USB_INPUT_HANDLE));
+    strncpy(SEND_BUFFER, (char*)message, messageSize);
+
+    int nextByteIndex = 0;
     while(nextByteIndex < messageSize) {
         while(USB_DEVICE.HandleBusy(USB_INPUT_HANDLE));
         int bytesToTransfer = min(USB_PACKET_SIZE,
                 messageSize - nextByteIndex);
         USB_INPUT_HANDLE = USB_DEVICE.GenWrite(DATA_ENDPOINT,
-                (uint8_t*)(message + nextByteIndex), bytesToTransfer);
+                (uint8_t*)(SEND_BUFFER + nextByteIndex), bytesToTransfer);
         nextByteIndex += bytesToTransfer;
     }
-    while(USB_DEVICE.HandleBusy(USB_INPUT_HANDLE));
 }
 
 void initializeUsb() {

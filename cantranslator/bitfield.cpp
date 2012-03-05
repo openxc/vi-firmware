7ff8dd5fcb6ce508f8c2c1c7d173c6c6c6b42e7d
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-03-05 16:10:50 -0500

    Refactor getBitField and test single byte access.

--- cantranslator/bitfield.cpp
+++ cantranslator/bitfield.cpp
@@ -1,45 +1,34 @@
 #include "bitfield.h"
 
-unsigned long getBitField(uint8_t* data, int startPos, int numBits) {
+unsigned long getBitField(uint8_t* data, int start_bit, int num_bits) {
   unsigned long ret = 0;
 
-  // Calculate the starting byte.
-  int startByte = startPos / 8;
-  int shift = 0;
-  
-  // Mask out any other bits besides those in the bitfield.
-  unsigned long bitmask = (unsigned long)((0x1 << numBits) - 1);
+  int start_byte = start_bit / 8;
 
-
-  if (numBits <= 8 ) {
-    //Calculate the starting bit position
-
-    int startBit = startPos % 8;
-    ret = data[startByte];
-
-    // New method: bit fields are positioned according to big-endian
-    // bit layout, but inside the bit field, values are represented
-    // as little-endian.
-    // Therefore, to get the bit field, we just need to convert to big-endian
-    // bit ordering to find the field, and directly use the value we find in
-    // the field.
-    // Calculate the end bit address of the field
-    int endBit = startBit + numBits;
-    shift = (8 - endBit);
-    ret = ret >> shift;
-    ret = (ret & bitmask);
+  if (num_bits <= 8 ) {
+      // Bit fields are positioned according to big-endian bit layout, but
+      // inside the bit field, values are represented as little-endian.
+      // Therefore, to get the bit field, we just need to convert to big-endian
+      // bit ordering to find the field, and directly use the value we find in
+      // the field.
+      int bit_position = start_bit % 8;
+      ret = data[start_byte];
+      int end_bit = bit_position + num_bits;
+      ret = ret >> (8 - end_bit);
   } else {
-    int endByte = (startPos + numBits) / 8;
+      int end_byte = (start_bit + num_bits) / 8;
 
-    // The lowest byte address contains the most significant bit.
-    for (int i = startByte; i <= endByte; i++) {
-	ret = ret << 8;
-	ret = ret | data[i];
+      // The lowest byte address contains the most significant bit.
+      for (int i = start_byte; i <= end_byte; i++) {
+          ret = ret << 8;
+          ret = ret | data[i];
       }
-      
-    shift = (8-((startPos +numBits) % 8)); //Calculates value to shift bitfield of interest to LSB
-    ret = ret >> shift;
-    ret = ret & bitmask;
+
+      //Calculates value to shift bitfield of interest to LSB
+      ret = ret >> (8 - ((start_bit + num_bits) % 8));
   }
-  return ret;
+
+  // Mask out any other bits besides those in the bitfield.
+  unsigned long bitmask = (unsigned long)((0x1 << num_bits) - 1);
+  return ret & bitmask;
 }
--- cantranslator/tests/bitfield_tests.c
+++ cantranslator/tests/bitfield_tests.c
@@ -2,17 +2,25 @@
 #include <stdint.h>
 #include "bitfield.h"
 
-START_TEST (test_size)
+START_TEST (test_one_byte)
 {
-    uint8_t data = 0xFF;
-    fail_unless(getBitField(&data, 0, 4) == 0xF);
+    uint8_t data = 0xFA;
+    unsigned long result = getBitField(&data, 0, 4);
+    fail_unless(result == 0xF,
+            "First 4 bits in 0x%X was 0x%X instead of 0xF", data, result);
+    result = getBitField(&data, 4, 4);
+    fail_unless(result == 0xA,
+            "First 4 bits in 0x%X was 0x%X instead of 0xA", data, result);
+    result = getBitField(&data, 0, 8);
+    fail_unless(result == 0xFA,
+            "All bits in 0x%X were 0x%X instead of 0x%X", data, result, data);
 }
 END_TEST
 
 Suite* bitfieldSuite(void) {
     Suite* s = suite_create("bitfield");
     TCase *tc_core = tcase_create("core");
-    tcase_add_test(tc_core, test_size);
+    tcase_add_test(tc_core, test_one_byte);
     suite_add_tcase(s, tc_core);
 
     return s;

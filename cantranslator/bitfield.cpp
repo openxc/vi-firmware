1700c4b727e0e0490afb974f44e95a5f211f2e51
Christopher Peplin <cpeplin@ford.com>
2012-03-08 14:23:51 -0500

    Revert naming change - we're using camelCase everywhere else.

--- cantranslator/bitfield.cpp
+++ cantranslator/bitfield.cpp
@@ -1,34 +1,34 @@
 #include "bitfield.h"
 
-unsigned long getBitField(uint8_t* data, int start_bit, int num_bits) {
+unsigned long getBitField(uint8_t* data, int startBit, int numBits) {
   unsigned long ret = 0;
 
-  int start_byte = start_bit / 8;
+  int startByte = startBit / 8;
 
-  if (num_bits <= 8 ) {
+  if (numBits <= 8 ) {
       // Bit fields are positioned according to big-endian bit layout, but
       // inside the bit field, values are represented as little-endian.
       // Therefore, to get the bit field, we just need to convert to big-endian
       // bit ordering to find the field, and directly use the value we find in
       // the field.
-      int bit_position = start_bit % 8;
-      ret = data[start_byte];
-      int end_bit = bit_position + num_bits;
-      ret = ret >> (8 - end_bit);
+      int bitPosition = startBit % 8;
+      ret = data[startByte];
+      int endBit = bitPosition + numBits;
+      ret = ret >> (8 - endBit);
   } else {
-      int end_byte = (start_bit + num_bits) / 8;
+      int endByte = (startBit + numBits) / 8;
 
       // The lowest byte address contains the most significant bit.
-      for (int i = start_byte; i <= end_byte; i++) {
+      for (int i = startByte; i <= endByte; i++) {
           ret = ret << 8;
           ret = ret | data[i];
       }
 
       //Calculates value to shift bitfield of interest to LSB
-      ret = ret >> (8 - ((start_bit + num_bits) % 8));
+      ret = ret >> (8 - ((startBit + numBits) % 8));
   }
 
   // Mask out any other bits besides those in the bitfield.
-  unsigned long bitmask = (unsigned long)((0x1 << num_bits) - 1);
+  unsigned long bitmask = (unsigned long)((0x1 << numBits) - 1);
   return ret & bitmask;
 }

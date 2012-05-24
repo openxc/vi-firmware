6c77a086f6d09580e266773d757bef132a1f6d6b
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-05-24 17:24:27 -0400

    Simplify the bitfield code futher by finding shared workflow.

--- cantranslator/bitfield.cpp
+++ cantranslator/bitfield.cpp
@@ -1,33 +1,40 @@
 #include "bitfield.h"
 
-unsigned long getBitField(uint8_t* data, int startBit, int numBits) {
-    unsigned long ret = 0;
-
-    int startByte = startBit / 8;
-    int endBit;
-
+/**
+ * Find the ending bit of a bitfield within the final byte.
+ *
+ * Returns: a bit position from 0 to 7.
+ */
+int findEndBit(int startBit, int numBits) {
     if (numBits <= 8 ) {
         // Bit fields are positioned according to big-endian bit layout, but
         // inside the bit field, values are represented as little-endian.
         // Therefore, to get the bit field, we just need to convert to big-endian
         // bit ordering to find the field, and directly use the value we find in
         // the field.
-        int bitPosition = startBit % 8;
-        ret = data[startByte];
-        endBit = bitPosition + numBits;
+        return (startBit % 8) + numBits;
     } else {
-        int endByte = (startBit + numBits) / 8;
+        // Calculates value to shift bitfield of interest to LSB
+        return (startBit + numBits) % 8;
+    }
+}
 
+unsigned long getBitField(uint8_t* data, int startBit, int numBits) {
+    unsigned long ret = 0;
+
+    int startByte = startBit / 8;
+    int endByte = (startBit + numBits - 1) / 8;
+
+    ret = data[startByte];
+    if(startByte != endByte) {
         // The lowest byte address contains the most significant bit.
-        for (int i = startByte; i <= endByte; i++) {
+        for (int i = startByte + 1; i <= endByte; i++) {
             ret = ret << 8;
             ret = ret | data[i];
         }
-
-        //Calculates value to shift bitfield of interest to LSB
-        endBit = ((startBit + numBits) % 8);
     }
-    ret = ret >> (8 - endBit);
+
+    ret = ret >> (8 - findEndBit(startBit, numBits));
 
     // Mask out any other bits besides those in the bitfield.
     unsigned long bitmask = (unsigned long)((0x1 << numBits) - 1);

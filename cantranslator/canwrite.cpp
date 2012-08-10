9757bb2ec722a86c40894c56fd60c4a089e23685
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-08-10 19:26:50 -0400

    Allow passing in existing message data to handlers.

--- cantranslator/canwrite.cpp
+++ cantranslator/canwrite.cpp
@@ -8,13 +8,24 @@ void checkWritePermission(CanSignal* signal, bool* send) {
 
 uint64_t booleanWriter(CanSignal* signal, CanSignal* signals,
         int signalCount, cJSON* value, bool* send) {
-    return encodeCanSignal(signal, value->valueint);
+    return booleanWriter(signal, signals, signalCount, value, send, 0);
+}
+
+uint64_t booleanWriter(CanSignal* signal, CanSignal* signals,
+        int signalCount, cJSON* value, bool* send, uint64_t data) {
+    checkWritePermission(signal, send);
+    return encodeCanSignal(signal, value->valueint, data);
 }
 
 uint64_t numberWriter(CanSignal* signal, CanSignal* signals,
         int signalCount, cJSON* value, bool* send) {
+    return numberWriter(signal, signals, signalCount, value, send, 0);
+}
+
+uint64_t numberWriter(CanSignal* signal, CanSignal* signals,
+        int signalCount, cJSON* value, bool* send, uint64_t data) {
     checkWritePermission(signal, send);
-    return encodeCanSignal(signal, value->valuedouble);
+    return encodeCanSignal(signal, value->valuedouble, data);
 }
 
 uint64_t stateWriter(CanSignal* signal, CanSignal* signals,
@@ -30,12 +41,15 @@ uint64_t stateWriter(CanSignal* signal, CanSignal* signals,
 }
 
 uint64_t encodeCanSignal(CanSignal* signal, float value) {
+    return encodeCanSignal(signal, value, 0);
+}
+
+uint64_t encodeCanSignal(CanSignal* signal, float value, uint64_t data) {
     unsigned long rawValue = (value - signal->offset) / signal->factor;
     if(rawValue > 0) {
         // round up to avoid losing precision when we cast to an int
         rawValue += 0.5;
     }
-    uint64_t data = 0;
     setBitField(&data, rawValue, signal->bitPosition, signal->bitSize);
     return data;
 }

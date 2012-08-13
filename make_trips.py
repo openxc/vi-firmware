be248eb2aea4a89d437fc7e941948b202329777f
David Melcher <dmelche6@ford.com>
2012-08-13 14:38:24 -0400

    Added make_trips.py to regroup trace fies into individual trips.

---
+++ make_trips.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+import sys
+from xml.etree import ElementTree as ET
+import json
+import re
+import datetime
+
+class DestFileCreator:
+    nextTrip = 1
+
+    def __init__(self, firstTrip):
+        self.nextTrip = firstTrip
+
+    def next_dest(self, data_file):
+        newName = "Trip" + str(self.nextTrip).zfill(3) + "-" + data_file
+        self.nextTrip = self.nextTrip + 1
+        print "Starting ",
+        print newName
+        return open(newName, "w")
+
+def get_next_file(trace_file):
+    numbers_str = re.findall(r'[0-9]+', trace_file)
+
+    numbers_int = map(int, numbers_str)
+
+    oldFile = datetime.datetime(numbers_int[0], numbers_int[1], numbers_int[2], numbers_int[3])
+
+    dtime = datetime.timedelta(hours=1)
+    
+    oldFile = oldFile + dtime
+
+    filename = str(oldFile.date())
+    filename += "-"
+    if(oldFile.hour < 10):
+        filename += "0"
+    filename += str(oldFile.hour)
+    filename += ".json"
+
+    return filename
+
+def compile_trip(trace_file, tripNum):
+    dataFileValid = True
+    lastTimeStamp = 0.0
+    currentTimeStamp = 0
+    destFileGen = DestFileCreator(tripNum)
+
+    destinationFile = destFileGen.next_dest(trace_file)
+
+    while dataFileValid is True:
+        try:
+            currentTraceFile = open(trace_file, "r")
+        except IOError, e:
+            print e
+            dataFileValid = False
+            destinationFile.close()
+            break
+        else:
+            print 'Opened %s' % trace_file
+
+            for line in currentTraceFile:
+                try:
+                    timestamp, data = line.split(':', 1)
+                    record = json.loads(data)
+                except ValueError:
+                    sys.stderr.write("Skipping line: %s" % data)
+                    print " "
+                    continue
+                
+                if lastTimeStamp is not 0.0:
+                    if (float(timestamp) - lastTimeStamp) > 600.00:  # Time is in seconds
+                        print "Found a gap of ",
+                        print (float(timestamp) - lastTimeStamp),
+                        print " seconds.  Creating new Trip file."
+                        destinationFile.close()
+                        lastTimeStamp = 0.0
+                        destinationFile = destFileGen.next_dest(trace_file)
+                    elif (float(timestamp) - lastTimeStamp) > 1.00:  # Time is in seconds
+                        print "Momentary dropout of ",
+                        print (float(timestamp) - lastTimeStamp),
+                        print " seconds.  Ignoring."
+                lastTimeStamp = float(timestamp)
+                destinationFile.write(line)
+                
+            if dataFileValid is True:
+                currentTraceFile.close()
+                trace_file = get_next_file(trace_file)
+
+
+def generate_gpx(trace_file):
+    root = ET.Element("gpx")
+
+    track = ET.SubElement(root, "trk")
+    number = ET.SubElement(track, "number")
+    number.text = "1"
+
+    latitude = None
+    longitude = None
+
+    segment = ET.SubElement(track, "trkseg")
+    for line in open(trace_file):
+        timestamp, data = line.split(':', 1)
+        try:
+            record = json.loads(data)
+        except ValueError:
+            sys.stderr.write("Skipping line: %s" % data)
+            continue
+
+        if record['name'] == 'latitude':
+            latitude = record['value']
+        elif record['name'] == 'longitude':
+            longitude = record['value']
+
+        if latitude and longitude:
+            point = ET.SubElement(segment, "trkpt")
+            point.set('lat', str(latitude))
+            point.set('lon', str(longitude))
+            latitude = longitude = None
+
+    return ET.tostring(ET.ElementTree(root).getroot())
+
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) is not 3:
+        print "Must provide the path to the first trace file in a trip and the trip number."
+        sys.exit(1)
+    
+    compile_trip(sys.argv[1], int(sys.argv[2]))

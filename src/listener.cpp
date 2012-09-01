f2bf01df0ecdb432ca1d2331d95ceca4e181c963
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-08-31 22:38:54 -0400

    Note a performance bottleneck.

--- src/listener.cpp
+++ src/listener.cpp
@@ -16,6 +16,10 @@ void conditionalEnqueue(QUEUE_TYPE(uint8_t)* queue, uint8_t* message,
 }
 
 void sendMessage(Listener* listener, uint8_t* message, int messageSize) {
+    // TODO the more we enqueue here, the slower it gets - cuts the rate by
+    // almost half to do it with 2 queues. we could either figure out how to
+    // share a queue or make the enqueue function faster. right now I believe it
+    // enqueues byte by byte, which could obviously be improved.
     conditionalEnqueue(&listener->usb->sendQueue, message, messageSize);
     conditionalEnqueue(&listener->serial->sendQueue, message, messageSize);
 }

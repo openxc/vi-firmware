dd7d24191ab60c6c9ff2404cc35d0e3f42138c43
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-08-31 22:33:47 -0400

    Share conditional enqueue logic between USB and serial.

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,8 +1,23 @@
 #include "listener.h"
+#include "log.h"
+#include "buffers.h"
+
+void conditionalEnqueue(QUEUE_TYPE(uint8_t)* queue, uint8_t* message,
+        int messageSize) {
+    if(queue_available(queue) < messageSize + 1) {
+            debug("Dropped incoming CAN message -- send queue full for USB");
+            return;
+    }
+
+    for(int i = 0; i < messageSize; i++) {
+        QUEUE_PUSH(uint8_t, queue, (uint8_t)message[i]);
+    }
+    QUEUE_PUSH(uint8_t, queue, (uint8_t)'\n');
+}
 
 void sendMessage(Listener* listener, uint8_t* message, int messageSize) {
-    sendMessage(listener->usb, message, messageSize);
-    sendMessage(listener->serial, message, messageSize);
+    conditionalEnqueue(&listener->usb->sendQueue, message, messageSize);
+    conditionalEnqueue(&listener->serial->sendQueue, message, messageSize);
 }
 
 void processListenerQueues(Listener* listener) {

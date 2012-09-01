56a14303d4730c24680be2ea4ca5da9e8fce4589
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-08-31 22:29:56 -0400

    Only queue a message to send over USB if queue has room for the whole thing.

--- src/usbutil.cpp
+++ src/usbutil.cpp
@@ -2,11 +2,13 @@
 #include "log.h"
 
 void sendMessage(UsbDevice* usbDevice, uint8_t* message, int messageSize) {
-    for(int i = 0; i < messageSize; i++) {
-        if(!QUEUE_PUSH(uint8_t, &usbDevice->sendQueue, (uint8_t)message[i])) {
+    if(queue_available(&usbDevice->sendQueue) < messageSize + 1) {
             debug("Dropped incoming CAN message -- send queue full for USB");
             return;
-        }
+    }
+
+    for(int i = 0; i < messageSize; i++) {
+        QUEUE_PUSH(uint8_t, &usbDevice->sendQueue, (uint8_t)message[i]);
     }
     QUEUE_PUSH(uint8_t, &usbDevice->sendQueue, (uint8_t)'\n');
 }

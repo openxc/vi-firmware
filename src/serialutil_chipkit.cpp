66e357f8e57d1169259514c16fb535ec24cfcb57
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-09-07 13:23:26 -0400

    Send more bytes per batch over serial.

--- src/serialutil_chipkit.cpp
+++ src/serialutil_chipkit.cpp
@@ -24,14 +24,10 @@ void initializeSerial(SerialDevice* serial) {
 void processInputQueue(SerialDevice* device) {
     int byteCount = 0;
     char sendBuffer[MAX_MESSAGE_SIZE];
-    while(!queue_empty(&device->sendQueue) && byteCount < 64) {
+    while(!queue_empty(&device->sendQueue) && byteCount < MAX_MESSAGE_SIZE) {
         sendBuffer[byteCount++] = QUEUE_POP(uint8_t, &device->sendQueue);
     }
 
-    // Serial transfers are really, really slow, so we don't want to send unless
-    // explicitly set to do so at compile-time. Alternatively, we could send
-    // on serial whenever we detect below that we're probably not connected to a
-    // USB device, but explicit is better than implicit.
     device->device->write((const uint8_t*)sendBuffer, byteCount);
 }
 

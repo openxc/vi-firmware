2c71787971a2bb9a8c652111e13ce3e2b967ddd2
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-09-09 13:24:14 -0400

    Add pin configuration for CAN2 on LPC1768.

--- src/canutil_lpc1768.cpp
+++ src/canutil_lpc1768.cpp
@@ -8,6 +8,12 @@
 
 #define CAN_CTRL(BUS) (BUS == LPC_CAN1 ? 0 : 1)
 
+// CAN1: select P0.0 as RD1. P0.1 as TD1
+// CAN2: select P2.7 as RD2, P2.8 as RD2
+#define CAN_RX_PIN_NUM(BUS) (BUS == LPC_CAN1 ? 0 : 7)
+#define CAN_TX_PIN_NUM(BUS) (BUS == LPC_CAN1 ? 1 : 8)
+#define CAN_PORT_NUM(BUS) (BUS == LPC_CAN1 ? 0 : 2)
+
 CAN_ERROR configureFilters(CanBus* bus, CanFilter* filters, int filterCount) {
     debug("Configuring %d filters...", filterCount);
     CAN_ERROR result = CAN_OK;
@@ -18,22 +24,23 @@ CAN_ERROR configureFilters(CanBus* bus, CanFilter* filters, int filterCount) {
     return result;
 }
 
-void initializeCan(CanBus* bus) {
-    queue_init(&bus->sendQueue);
-
-    // TODO need to store pin configurations in the struct
-    /* Pin configuration
-     * CAN1: select P0.0 as RD1. P0.1 as TD1
-     */
+void configureCanControllerPins(LPC_CAN_TypeDef* controller) {
     PINSEL_CFG_Type PinCfg;
     PinCfg.Funcnum = 1;
     PinCfg.OpenDrain = 0;
     PinCfg.Pinmode = 0;
-    PinCfg.Pinnum = 0;
-    PinCfg.Portnum = 0;
+    PinCfg.Pinnum = CAN_RX_PIN_NUM(controller);
+    PinCfg.Portnum = CAN_PORT_NUM(controller);
     PINSEL_ConfigPin(&PinCfg);
-    PinCfg.Pinnum = 1;
+
+    PinCfg.Pinnum = CAN_TX_PIN_NUM(controller);
     PINSEL_ConfigPin(&PinCfg);
+}
+
+void initializeCan(CanBus* bus) {
+    queue_init(&bus->sendQueue);
+
+    configureCanControllerPins(bus->controller);
 
     // the bus is coming up as 0 in the struct for some reason
     CAN_Init(bus->controller, bus->speed);

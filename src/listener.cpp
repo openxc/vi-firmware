4893de077e29e9e27f6218a717eff6e98261df0a
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-09-09 13:33:22 -0400

    Remove TODO - problem was debug() printing, not enqueing.

--- src/listener.cpp
+++ src/listener.cpp
@@ -18,10 +18,6 @@ void conditionalEnqueue(QUEUE_TYPE(uint8_t)* queue, uint8_t* message,
 }
 
 void sendMessage(Listener* listener, uint8_t* message, int messageSize) {
-    // TODO the more we enqueue here, the slower it gets - cuts the rate by
-    // almost half to do it with 2 queues. we could either figure out how to
-    // share a queue or make the enqueue function faster. right now I believe it
-    // enqueues byte by byte, which could obviously be improved.
     if(listener->usb->configured) {
         conditionalEnqueue(&listener->usb->sendQueue, message, messageSize);
     } else {

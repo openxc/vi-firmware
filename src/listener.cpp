52de4e0e6a00a475325d7f445f8e9a04dc52f68e
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-09-11 09:59:25 -0400

    Print a queue-specific debug message when dropping CAN messages.

--- src/listener.cpp
+++ src/listener.cpp
@@ -2,12 +2,10 @@
 #include "log.h"
 #include "buffers.h"
 
-void conditionalEnqueue(QUEUE_TYPE(uint8_t)* queue, uint8_t* message,
+bool conditionalEnqueue(QUEUE_TYPE(uint8_t)* queue, uint8_t* message,
         int messageSize) {
     if(queue_available(queue) < messageSize + 2) {
-        debug("Dropped incoming CAN message -- send queue (at %p) full\r\n",
-                queue);
-        return;
+        return false;
     }
 
     for(int i = 0; i < messageSize; i++) {
@@ -15,11 +13,17 @@ void conditionalEnqueue(QUEUE_TYPE(uint8_t)* queue, uint8_t* message,
     }
     QUEUE_PUSH(uint8_t, queue, (uint8_t)'\r');
     QUEUE_PUSH(uint8_t, queue, (uint8_t)'\n');
+    return true;
 }
 
 void sendMessage(Listener* listener, uint8_t* message, int messageSize) {
-    conditionalEnqueue(&listener->usb->sendQueue, message, messageSize);
-    conditionalEnqueue(&listener->serial->sendQueue, message, messageSize);
+    if(!conditionalEnqueue(&listener->usb->sendQueue, message, messageSize)) {
+        debug("USB send queue full, dropping CAN message: %s\r\n", message);
+    }
+
+    if(!conditionalEnqueue(&listener->serial->sendQueue, message, messageSize)) {
+        debug("UART send queue full, dropping CAN message: %s\r\n", message);
+    }
 }
 
 void processListenerQueues(Listener* listener) {

a5bed169009f4f91b7bb7184e521a6e5bcc5913d
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-09-11 21:43:40 -0400

    Warn user if chipKIT libraries are missing and give download link.
    Unfortunately we are unable to either simply include the libraries in this
    repository or even link to a git repo hosted elsewhere because of the license
    Microchip put on this code. The user has to download manually and agree to the
    license at Digilent's website.

--- src/pic32.mk
+++ src/pic32.mk
@@ -20,5 +20,17 @@ endif
 
 EXTRA_CXXFLAGS += -G0 -D__PIC32__ $(CC_SYMBOLS)
 
+CHIPKIT_LIBRARY_AGREEMENT_URL = http://www.digilentinc.com/Agreement.cfm?DocID=DSD-0000318
+MICROCHIP_CAN_LIBRARY_EXISTS = $(shell test -d libs/chipKITCAN; echo $$?)
+ifneq ($(MICROCHIP_CAN_LIBRARY_EXISTS),0)
+$(error chipKIT CAN library missing - download separately from $(CHIPKIT_LIBRARY_AGREEMENT_URL) and place at ./libs/chipKITCAN)
+endif
+
+MICROCHIP_USB_LIBRARY_EXISTS = $(shell test -d libs/chipKITUSBDevice; echo $$?)
+ifneq ($(MICROCHIP_USB_LIBRARY_EXISTS),0)
+$(error chipKIT USB device library missing - download separately from $(CHIPKIT_LIBRARY_AGREEMENT_URL) and place at ./libs/chipKITUSBDevice)
+endif
+
+USER_LIB_PATH = ./libs
 ARDUINO_MAKEFILE_HOME = libs/arduino.mk
 include $(ARDUINO_MAKEFILE_HOME)/chipKIT.mk

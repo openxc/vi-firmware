a5860b6c29271d9007a1df7b7904d4181c9ae007
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-09-11 22:05:47 -0400

    Split logging definitions up into separate files for each platform.

--- src/log.h
+++ src/log.h
@@ -1,13 +1,18 @@
 #ifndef _LOG_H_
 #define _LOG_H_
 
-#include <stdarg.h>
-#include <stdio.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define MAX_LOG_LINE_LENGTH 120
 
 void initializeLogging();
 
 void debug(const char* format, ...);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // _LOG_H_
---
+++ src/log_chipkit.cpp
@@ -0,0 +1,25 @@
+#ifdef __PIC32__
+
+#include "log.h"
+#include "WProgram.h"
+#include <stdio.h>
+
+void debug(const char* format, ...) {
+#ifdef __DEBUG__
+    va_list args;
+    va_start(args, format);
+
+    char buffer[MAX_LOG_LINE_LENGTH];
+    vsnprintf(buffer, MAX_LOG_LINE_LENGTH, format, args);
+
+    Serial.print(buffer);
+
+    va_end(args);
+#endif // __DEBUG__
+}
+
+void initializeLogging() {
+    Serial.begin(115200);
+}
+
+#endif // __PIC32__

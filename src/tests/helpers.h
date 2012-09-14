ccca262af89bc7df05c1c64f31956fda41b2a3af
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-09-13 23:57:24 -0400

    Fix test build with new pure C files.

--- src/tests/helpers.h
+++ src/tests/helpers.h
@@ -1,6 +1,14 @@
 #ifndef _HELPERS_H
 #define _HELPERS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void check_equal_unit64(uint64_t value, uint64_t expected);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // _HELPERS_H_

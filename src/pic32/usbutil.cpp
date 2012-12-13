090160b3f02ac28da557f45a019ab135ca7a1844
Christopher Peplin <cpeplin@ford.com>
2012-12-13 14:29:45 -0500

    Silence a really noise log message.

--- src/pic32/usbutil.cpp
+++ src/pic32/usbutil.cpp
@@ -45,8 +45,10 @@ bool waitForHandle(UsbDevice* usbDevice) {
     while(usbDevice->device.HandleBusy(usbDevice->deviceToHostHandle)) {
         ++i;
         if(i > 800) {
-            debug("USB most likely not connected or at least not requesting "
-                    "IN transfers - bailing out of handle waiting\r\n");
+            // This can get really noisy when running but I want to leave it in
+            // because it' useful to enable when debugging.
+            // debug("USB most likely not connected or at least not requesting "
+                    // "IN transfers - bailing out of handle waiting\r\n");
             return false;
         }
     }

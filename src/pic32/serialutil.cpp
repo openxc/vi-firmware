82269eeb45a55d91957c83296567f73c39b3d872
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-12-17 10:28:20 -0500

    Pull up UART baud rate to a named constant.

--- src/pic32/serialutil.cpp
+++ src/pic32/serialutil.cpp
@@ -3,6 +3,8 @@
 #include "log.h"
 #include "WProgram.h"
 
+#define UART_BAUDRATE 115200
+
 // TODO see if we can do this with interrupts on the chipKIT
 // http://www.chipkit.org/forum/viewtopic.php?f=7&t=1088
 void readFromSerial(SerialDevice* device, bool (*callback)(uint8_t*)) {
@@ -19,7 +21,7 @@ void readFromSerial(SerialDevice* device, bool (*callback)(uint8_t*)) {
 
 void initializeSerial(SerialDevice* device) {
 	device->controller = &Serial1;
-    ((HardwareSerial*)device->controller)->begin(115200);
+    ((HardwareSerial*)device->controller)->begin(UART_BAUDRATE);
     QUEUE_INIT(uint8_t, &device->receiveQueue);
     QUEUE_INIT(uint8_t, &device->sendQueue);
 }

c7fd3edb1d03c28eb99d12897b2024e76cdecba5
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-12-18 22:03:57 -0500

    Avoid multiple method definitions of strutil functions on PIC32.

--- src/strutil.c
+++ src/strutil.c
@@ -1,6 +1,6 @@
 #include "strutil.h"
 
-#if defined(NO_ETHERNET) || !defined(__PIC32__)
+#ifndef __PIC32__
 
 const char *strnchr(const char *str, size_t len, char character) {
     const char *end = str + len;
@@ -12,4 +12,4 @@ const char *strnchr(const char *str, size_t len, char character) {
     return NULL;
 }
 
-#endif
+#endif // __PIC32__
--- src/strutil.h
+++ src/strutil.h
@@ -11,7 +11,8 @@ extern "C" {
 // the same name and the same functionality "strnchr".
 // Therefore this section should not be compiled when
 // the Ethernet library is included.
-#if defined(NO_ETHERNET) || !defined(__PIC32__)
+#ifndef __PIC32__
+
 /*
  * Thanks to https://gist.github.com/855214.
  */

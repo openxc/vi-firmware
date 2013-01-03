1f9fda06eb7cd905708a3506f5cd9c84e1e78408
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-01-03 15:03:30 -0500

    Log the current operating mode on startup.

--- src/cantranslator.cpp
+++ src/cantranslator.cpp
@@ -25,6 +25,14 @@ bool receiveWriteRequest(uint8_t*);
 
 void setup() {
     initializeLogging();
+
+    debug("Initializing as a CAN ");
+#ifdef TRANSMITTER
+    debug("transmitter\r\n");
+#else
+    debug("translator\r\n");
+#endif // TRANSMITTER
+
     initializeUsb(listener.usb);
     if(listener.serial != NULL) {
         initializeSerial(listener.serial);

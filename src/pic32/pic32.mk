5888177b6b8eadb07b1a5f3f6742b75403b1210e
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-01-04 19:11:43 -0500

    Don't build CAN library into .bin if we're not using it (i.e. emulator).

--- src/pic32/pic32.mk
+++ src/pic32/pic32.mk
@@ -1,9 +1,13 @@
 BOARD_TAG = mega_pic32
 TARGET = $(BASE_TARGET)-pic32
 
-ARDUINO_LIBS = chipKITCAN chipKITUSBDevice chipKITUSBDevice/utility cJSON
+ARDUINO_LIBS = chipKITUSBDevice chipKITUSBDevice/utility cJSON
 ifdef ETHERNET
 ARDUINO_LIBS += chipKITEthernet chipKITEthernet/utility
+endif
+
+ifndef CAN_EMULATOR
+ARDUINO_LIBS += chipKITCAN
 endif
 
 NO_CORE_MAIN_FUNCTION = 1
@@ -26,16 +30,18 @@ EXTRA_CPPFLAGS += -G0 -D__PIC32__ $(CC_SYMBOLS)
 
 CHIPKIT_LIBRARY_AGREEMENT_URL = http://www.digilentinc.com/Agreement.cfm?DocID=DSD-0000318
 
+EXPECTED_USB_LIBRARY_PATH = ./libs/chipKITUSBDevice
+MICROCHIP_USB_LIBRARY_EXISTS = $(shell test -d $(EXPECTED_USB_LIBRARY_PATH); echo $$?)
+ifneq ($(MICROCHIP_USB_LIBRARY_EXISTS),0)
+$(error chipKIT USB device library missing - download separately from $(CHIPKIT_LIBRARY_AGREEMENT_URL) and place at $(EXPECTED_USB_LIBRARY_PATH))
+endif
+
+ifndef CAN_EMULATOR
 EXPECTED_CAN_LIBRARY_PATH = ./libs/chipKITCAN
 MICROCHIP_CAN_LIBRARY_EXISTS = $(shell test -d $(EXPECTED_CAN_LIBRARY_PATH); echo $$?)
 ifneq ($(MICROCHIP_CAN_LIBRARY_EXISTS),0)
 $(error chipKIT CAN library missing - download separately from $(CHIPKIT_LIBRARY_AGREEMENT_URL) and place at $(EXPECTED_CAN_LIBRARY_PATH))
 endif
-
-EXPECTED_USB_LIBRARY_PATH = ./libs/chipKITUSBDevice
-MICROCHIP_USB_LIBRARY_EXISTS = $(shell test -d $(EXPECTED_USB_LIBRARY_PATH); echo $$?)
-ifneq ($(MICROCHIP_USB_LIBRARY_EXISTS),0)
-$(error chipKIT USB device library missing - download separately from $(CHIPKIT_LIBRARY_AGREEMENT_URL) and place at $(EXPECTED_USB_LIBRARY_PATH))
 endif
 
 ifdef ETHERNET

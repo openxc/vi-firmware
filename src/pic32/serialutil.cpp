cfb261dd6400ca25c60122ff4165f7f37e31fdd5
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-01-06 14:58:25 -0500

    Manually flip UART register bits to enable flow control.
    This commit also switches the UART port used from UART1 to UART2, because the
    CTS/RTS lines aren't exposed on the chipKIT for UART1.

--- src/pic32/serialutil.cpp
+++ src/pic32/serialutil.cpp
@@ -1,3 +1,17 @@
+/* UART interface for PIC32.
+ *
+ * On the PIC32, the CAN translator uses Serial2 from the chipKIT library's
+ * HardwareSerial interface. This is equivalent to UART2, UART3A or U3A,
+ * depending on which piece of Digilent or Microchip documentation you happen
+ * to be using.
+ *
+ * Hardware flow control (RTS/CTS) is enabled, so CTS must be pulled low by the receiving device before data will be sent.
+ *
+ * Pin 14 - U3ARTS, connect this to the CTS line of the receiver.
+ * Pin 15 - U3ACTS, connect this to the RTS line of the receiver.
+ * Pin 16 - U3ATX, connect this to the RX line of the receiver.
+ * Pin 17 - U3ARX, connect this to the TX line of the receiver.
+ */
 #include "serialutil.h"
 #include "buffers.h"
 #include "log.h"
@@ -5,7 +19,10 @@
 
 #define UART_BAUDRATE 115200
 
-extern HardwareSerial Serial1;
+// bit 8 in the uxMode register controls hardware flow control
+#define _UARTMODE_FLOWCONTROL 8
+
+extern HardwareSerial Serial2;
 
 // TODO see if we can do this with interrupts on the chipKIT
 // http://www.chipkit.org/forum/viewtopic.php?f=7&t=1088
@@ -26,8 +43,13 @@ void readFromSerial(SerialDevice* device, bool (*callback)(uint8_t*)) {
 void initializeSerial(SerialDevice* device) {
     if(device != NULL) {
         initializeSerialCommon(device);
-        device->controller = &Serial1;
+        device->controller = &Serial2;
         ((HardwareSerial*)device->controller)->begin(UART_BAUDRATE);
+        // Manually enable RTS/CTS hardware flow control using the UART2
+        // register. The chipKIT serial library doesn't have an interface to do
+        // this, and the alternative UART libraries provided by Microchip are a
+        // bit of a mess.
+        ((p32_uart*)_UART2_BASE_ADDRESS)->uxMode.reg |= 2 << _UARTMODE_FLOWCONTROL;
     }
 }
 

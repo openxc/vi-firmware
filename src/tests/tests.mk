b387f7acfe6a9a33d77bcfca1be53ac58328b025
Christopher Peplin <cpeplin@ford.com>
2013-01-11 18:47:02 -0500

    Expand bootstrap script to support Cygwin in Windows

--- src/tests/tests.mk
+++ src/tests/tests.mk
@@ -52,9 +52,10 @@ code_generation_test:
 	@if [ -h signals.cpp ]; then mv -f signals.cpp signals.cpp.bak; fi
 	@if [ -h handlers.cpp ]; then mv -f handlers.cpp handlers.cpp.bak; fi
 	@if [ -h handlers.h ]; then mv -f handlers.h handlers.h.bak; fi
-	@ln -s $(TEST_OBJDIR)/signals.cpp
-	@ln -s handlers.cpp.example handlers.cpp
-	@ln -s handlers.h.example handlers.h
+    # Ideally we would symlink these files, but symlinks don't work well in Cygwin
+	@cp $(TEST_OBJDIR)/signals.cpp signals.cpp
+	@cp handlers.cpp.example handlers.cpp
+	@cp handlers.h.example handlers.h
 
 COVERAGE_INFO_FILENAME = coverage.info
 COVERAGE_INFO_PATH = $(TEST_OBJDIR)/$(COVERAGE_INFO_FILENAME)

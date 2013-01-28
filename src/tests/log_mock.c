a77891726ba57d03abdc2153d38d4c2f45a0d623
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-01-28 11:31:59 -0500

    Enable printf in unit tests regardless of DEBUG flag status.

--- src/tests/log_mock.c
+++ src/tests/log_mock.c
@@ -1,10 +1,12 @@
 #include "log.h"
 #include <stdio.h>
+#include <stdarg.h>
 
 void debug(const char* format, ...) {
-#ifdef __DEBUG__
+    va_list args;
+    va_start(args, format);
     vprintf(format, args);
-#endif // __DEBUG__
+    va_end(args);
 }
 
 void initializeLogging() { }

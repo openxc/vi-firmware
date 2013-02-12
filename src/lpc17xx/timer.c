66ec6a902d9c26649028d1bb2f3f4a676c2f25ad
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-02-12 18:26:43 -0500

    Fix test suite by adding timer mock.

--- src/lpc17xx/timer.c
+++ src/lpc17xx/timer.c
@@ -2,7 +2,7 @@
 #include "lpc_types.h"
 #include "timer.h"
 
-void delayMs(uint32_t delayInMs) {
+void delayMs(int delayInMs) {
 	LPC_TIM0->TCR = 0x02;		/* reset timer */
 	LPC_TIM0->PR  = 0x00;		/* set prescaler to zero */
 	LPC_TIM0->MR0 = (SystemCoreClock / 4) / (1000 / delayInMs);  //enter delay time

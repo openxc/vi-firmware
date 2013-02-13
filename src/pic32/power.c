2d9162a90b87be87398789b58c93390d92a66e93
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-02-13 11:24:09 -0500

    Add functions to control power settings of board, plus implementation for ARM.
    The Ford ARM board has a 12v power passthrough to power additional devices from
    the car, and there's a switch we can control from a GPIO port so it doesn't
    drain the battery when the car is off.

---
+++ src/pic32/power.c
@@ -0,0 +1,7 @@
+#include "power.h"
+
+void initializePower() {
+}
+
+void updatePower() {
+}
---
+++ src/power.h
@@ -0,0 +1,16 @@
+#ifndef _POWER_H_
+#define _POWER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void initializePower();
+
+void updatePower();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _POWER_H_

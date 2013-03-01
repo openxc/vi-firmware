cd66f3a9f92a13328cb00bf4bd26ee5a4be5b0b7
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-03-01 17:46:19 -0500

    Control the green user LED on the chipKIT (show CAN activity).

--- src/pic32/lights.cpp
+++ src/pic32/lights.cpp
@@ -1,11 +1,23 @@
-#include "timer.h"
 #include "lights.h"
-#include "log.h"
+#include "gpio.h"
+
+#define USER_LED_PIN 13
 
 void enable(Light light, RGB color) {
-    // TODO
+    GpioValue value;
+    if(color.r == 0 && color.g == 0 && color.b == 0) {
+        value = GPIO_VALUE_LOW;
+    } else {
+        value = GPIO_VALUE_HIGH;
+    }
+
+    switch(light) {
+        case LIGHT_A:
+            setGpioValue(0, USER_LED_PIN, value);
+            break;
+    }
 }
 
 void initializeLights() {
-    // TODO
+    setGpioDirection(0, USER_LED_PIN, GPIO_DIRECTION_OUTPUT);
 }

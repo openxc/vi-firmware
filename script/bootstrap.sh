8d2ec79018e081046be614d513be75b3c3635bbc
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-04-24 22:54:58 -0400

    Fix link for chipKIT libraries in bootstrap.

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -102,7 +102,7 @@ echo "Installing dependencies for building for chipKIT Max32 platform"
 ## chipKIT libraries for USB, CAN and Network
 
 CHIPKIT_LIBRARY_AGREEMENT_URL="http://www.digilentinc.com/Agreement.cfm?DocID=DSD-0000318"
-CHIPKIT_LIBRARY_DOWNLOAD_URL="http://www.digilentinc.com/Data/Documents/Product%20Documentation/chipKIT%20Ethernet%20and%20USB%20Libs.zip"
+CHIPKIT_LIBRARY_DOWNLOAD_URL="http://www.digilentinc.com/Data/Documents/Product%20Documentation/chipKIT%20Network%20and%20USB%20Libs.zip"
 CHIPKIT_ZIP_FILE="chipkit.zip"
 
 _pushd $DEPENDENCIES_FOLDER

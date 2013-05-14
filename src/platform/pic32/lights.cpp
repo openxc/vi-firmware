683c3246261e6b802bec54a9637b90505e296dea
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-05-14 13:56:39 -0400

    Share code in PIC32's light support.

--- src/platform/pic32/lights.cpp
+++ src/platform/pic32/lights.cpp
@@ -4,17 +4,19 @@
 #if defined(FLEETCARMA)
 
     #define USER_LED_A_SUPPORT
-    #define USER_LED_A_POLARITY    0        // turn on LED = drive pin low
-    #define USER_LED_A_PIN        3        // PORTD BIT0 (RD0) = GREEN
+    #define USER_LED_A_POLARITY 0 // turn on LED = drive pin low
+    #define USER_LED_A_PIN 3 // PORTD BIT0 (RD0) = GREEN
 
     #define USER_LED_B_SUPPORT
-    #define USER_LED_B_POLARITY    0        // turn on LED = drive pin low
-    #define USER_LED_B_PIN        4        // PORTC BIT14 (RC14) = BLUE
+    #define USER_LED_B_POLARITY 0 // turn on LED = drive pin low
+    #define USER_LED_B_PIN 4 // PORTC BIT14 (RC14) = BLUE
 
 #elif defined(CHIPKIT)
+
     #define USER_LED_A_SUPPORT
     #define USER_LED_A_POLARITY    1        // turn on LED = drive pin high
     #define USER_LED_A_PIN         13
+
 #endif
 
 using openxc::gpio::GpioValue;
@@ -22,32 +24,31 @@ using openxc::gpio::GPIO_VALUE_LOW;
 using openxc::gpio::GPIO_VALUE_HIGH;
 using openxc::gpio::GPIO_DIRECTION_OUTPUT;
 
+void enablePin(openxc::lights::RGB color, int pin, int polarity) {
+    GpioValue value;
+    if(color.r == 0 && color.g == 0 && color.b == 0) {
+        value = polarity ? GPIO_VALUE_LOW : GPIO_VALUE_HIGH;
+    } else {
+        value = polarity ? GPIO_VALUE_HIGH : GPIO_VALUE_LOW;
+    }
+    setGpioValue(0, pin, value);
+}
+
 void openxc::lights::enable(Light light, RGB color) {
     GpioValue value;
 
-
     switch(light) {
-        #if defined(USER_LED_A_SUPPORT)
+#if defined(USER_LED_A_SUPPORT)
         case LIGHT_A:
-            if(color.r == 0 && color.g == 0 && color.b == 0) {
-                value = USER_LED_A_POLARITY ? GPIO_VALUE_LOW : GPIO_VALUE_HIGH;
-            } else {
-                value = USER_LED_A_POLARITY ? GPIO_VALUE_HIGH : GPIO_VALUE_LOW;
-            }
-            setGpioValue(0, USER_LED_A_PIN, value);
+            enablePin(color, USER_LED_A_PIN, USER_LED_A_POLARITY);
             break;
-        #endif
+#endif
 
-        #if defined(USER_LED_B_SUPPORT)
+#if defined(USER_LED_B_SUPPORT)
         case LIGHT_B:
-            if(color.r == 0 && color.g == 0 && color.b == 0) {
-                value = USER_LED_B_POLARITY ? GPIO_VALUE_LOW : GPIO_VALUE_HIGH;
-            } else {
-                value = USER_LED_B_POLARITY ? GPIO_VALUE_HIGH : GPIO_VALUE_LOW;
-            }
-            setGpioValue(0, USER_LED_B_PIN, value);
+            enablePin(color, USER_LED_B_PIN, USER_LED_B_POLARITY);
             break;
-            #endif
+#endif
         default:
             break;
     }

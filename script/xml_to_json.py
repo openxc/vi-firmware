a21e089f03f2f333a2a34f8e1017e9c564c3dc84
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-06-04 16:52:34 -0400

    Refactor, simplify an speed up XML to JSON conversion.
    It now expects the new format, as documented in the sphinx docs. It's
    also now built so that you can do the conversion within Python, without
    writing out to a file.

--- script/xml_to_json.py
+++ script/xml_to_json.py
@@ -3,53 +3,56 @@
 import sys
 import argparse
 import json
-from xml.etree.ElementTree import parse
+from lxml import etree
 
-from generate_code import JsonParser, Signal
+from generate_code import Signal
+
+def fatal_error(message):
+    # TODO add red color
+    sys.stderr.write("ERROR: %s\n" % message)
+    sys.exit(1)
+
+
+def warning(message):
+    # TODO add yellow color
+    sys.stderr.write("WARNING: %s\n" % message)
 
 
 class Network(object):
     """Represents all the messages on a single bus."""
 
-    def __init__(self, tree, address, all_messages=None):
-        self.address = address
+    def __init__(self, tree, all_messages):
         self.messages = {}
 
-        for node in tree.getroot().findall("Node"):
-            self._parse_node(node, all_messages)
+        for message_id, message in all_messages.items():
+            node = tree.find("/Node/TxMessage[ID=\"%s\"]" % message_id)
+            if node is None:
+                warning("Unable to find message ID %d in XML" % message_id)
+            else:
+                self.messages[message_id] = Message(node, message['signals'])
 
     def to_dict(self):
-        return {self.address: {"messages": dict(("0x%x" % message.id,
+        return {"messages": dict(("0x%x" % message.id,
                     message.to_dict())
                 for message in list(self.messages.values())
-                if len(message.signals) > 0)}}
-
-    def _parse_node(self, node, all_messages):
-        # Looks like RxMessage elements are redundant.
-        for message_node in node.findall("TxMessage"):
-            message = Message(message_node, all_messages)
-            self.messages[message.id] = message
+                if len(message.signals) > 0)}
 
 
 class Message(object):
     """Contains a single CAN message."""
 
-    def __init__(self, node, all_messages=None):
+    def __init__(self, node, mapped_signals):
         self.signals = []
 
-        # XXX Is the number of bytes in DLC?
         self.name = node.find("Name").text
         self.id = int(node.find("ID").text, 0)
 
-        for message in all_messages:
-            if message.id == self.id:
-                signals = (Signal.from_xml_node(signal_node)
-                        for signal_node in node.findall("Signal"))
-                for signal in signals:
-                    for candidate in message.signals:
-                        if candidate.name == signal.name:
-                            signal.generic_name = candidate.generic_name
-                            self.signals.append(signal)
+        for signal_name in mapped_signals.keys():
+            mapped_signal_node = node.find("Signal[Name=\"%s\"]" % signal_name)
+            if mapped_signal_node is not None:
+                mapped_signal = Signal.from_xml_node(mapped_signal_node)
+                mapped_signal.generic_name = signal_name
+                self.signals.append(mapped_signal)
 
     def to_dict(self):
         return {"name": self.name,
@@ -60,7 +63,7 @@ class Message(object):
 def parse_options(argv):
     parser = argparse.ArgumentParser(
             description="Convert Canoe XML to the OpenXC JSON format.")
-    parser.add_argument("xml", help="Name of Canoe XML file")
+    parser.add_argument("database", help="Name of Canoe XML file")
     parser.add_argument("mapping_filename",
             help="Path to a JSON file with CAN messages mapped to OpenXC names")
     parser.add_argument("out", default="dump.json",
@@ -69,26 +72,26 @@ def parse_options(argv):
     return parser.parse_args(argv)
 
 
+def convert_to_json(database_filename, messages):
+    if len(messages) == 0:
+        warning("No messages specified for mapping from XML")
+        return {}
+    else:
+        tree = etree.parse(database_filename)
+        return Network(tree, messages).to_dict()
+
+
 def main(argv=None):
     arguments = parse_options(argv)
 
-    parser = JsonParser(arguments.mapping_filename)
-    parser.parse()
-    message_count = sum((len(bus.get('messages', {}))
-            for bus in list(parser.buses.values())))
-    if len(parser.buses) > 1:
-        raise RuntimeError("No more than one CAN bus can be defined")
-    elif len(parser.buses) == 0 or message_count == 0:
-        data = {}
-    else:
-        tree = parse(arguments.xml)
-        bus_address, bus = list(parser.buses.items())[0]
-        n = Network(tree, bus_address, bus['messages'])
-        data = {'buses': n.to_dict()}
+    with open(arguments.mapping_filename, 'r') as mapping_file:
+        mapping = json.load(mapping_file)
+        data = convert_to_json(arguments.database, mapping.get('messages', []))
 
     with open(arguments.out, 'w') as output_file:
         json.dump(data, output_file, indent=4)
     print("Wrote results to %s" % arguments.out)
+
 
 if __name__ == "__main__":
     sys.exit(main())

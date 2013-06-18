df2b119c8f01a566318e09e680e0b3e7b81b58bb
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-06-18 14:36:27 -0400

    Keep UART init logging on one line, as USB does.

--- src/interface/uart.cpp
+++ src/interface/uart.cpp
@@ -4,9 +4,11 @@
 
 const int openxc::interface::uart::MAX_MESSAGE_SIZE = 128;
 
+using openxc::util::log::debugNoNewline;
+
 void openxc::interface::uart::initializeCommon(UartDevice* device) {
     if(device != NULL) {
-        debug("Initializing UART.....");
+        debugNoNewline("Initializing UART.....");
         QUEUE_INIT(uint8_t, &device->receiveQueue);
         QUEUE_INIT(uint8_t, &device->sendQueue);
     }

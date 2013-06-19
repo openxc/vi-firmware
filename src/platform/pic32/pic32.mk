7549b11a7c7ab5a6d7ceb2939371591dce4ca3e4
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-06-19 15:20:59 -0400

    Restore serial port detection by reverting an overzealous regex.

--- src/platform/pic32/pic32.mk
+++ src/platform/pic32/pic32.mk
@@ -49,22 +49,22 @@ UART_BAUDRATE = 115200
 
 OSTYPE := $(shell uname)
 
-ifndef UART_PORT
+ifndef SERIAL_PORT
 	# Backwards compatibility with people using old name for this
 	ifdef ARDUINO_PORT
-		UART_PORT := $(ARDUINO_PORT)
+		SERIAL_PORT := $(ARDUINO_PORT)
 	endif
 endif
 
-ifndef UART_PORT
+ifndef SERIAL_PORT
 	ifeq ($(OSTYPE),Darwin)
-		UART_PORT = /dev/tty.usbserial*
+		SERIAL_PORT = /dev/tty.usbserial*
 	else
 		OSTYPE := $(shell uname -o)
 		ifeq ($(OSTYPE),Cygwin)
-			UART_PORT = com3
+			SERIAL_PORT = com3
 		else
-			UART_PORT = /dev/ttyUSB*
+			SERIAL_PORT = /dev/ttyUSB*
 		endif
 	endif
 endif

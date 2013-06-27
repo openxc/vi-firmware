6e7664b284c231b9b925d2a235bbd5afe6886202
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-06-27 11:59:40 -0400

    Use a hacky method to determine if root is necessary to bootstrap a Python package.

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -283,8 +283,14 @@ if ! command -v pip >/dev/null 2>&1; then
     if ! command -v easy_install >/dev/null 2>&1; then
         die "easy_install not available, can't install pip"
     fi
-    # TODO root may not be required here for some people
-    sudo easy_install pip
+
+    # TODO this is kind of a hacky way of determining if root is required -
+    # ideally we wouuld set up a little virtualenv in the dependencies folder
+    SUDO_CMD=
+    if ! command -v sudo >/dev/null 2>&1; then
+        SUDO_CMD=sudo
+    fi
+    $SUDO_CMD easy_install pip
 fi
 
 if ! python -c "import openxc"; then

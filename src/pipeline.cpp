6784693531795f2074525e1b60d4d1b8abb31c3b
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-09-04 18:00:14 -0400

    Log pipeline queue statistics.

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -33,6 +33,8 @@ const char messageTypeNames[][9] = {
 unsigned int droppedMessages[PIPELINE_ENDPOINT_COUNT];
 unsigned int sentMessages[PIPELINE_ENDPOINT_COUNT];
 unsigned int dataSent[PIPELINE_ENDPOINT_COUNT];
+unsigned int sendQueueLength[PIPELINE_ENDPOINT_COUNT];
+unsigned int receiveQueueLength[PIPELINE_ENDPOINT_COUNT];
 
 void openxc::pipeline::sendMessage(Pipeline* pipeline, uint8_t* message,
         int messageSize) {
@@ -44,6 +46,8 @@ void openxc::pipeline::sendMessage(Pipeline* pipeline, uint8_t* message,
             ++sentMessages[USB];
             dataSent[USB] += messageSize;
         }
+        sendQueueLength[USB] = QUEUE_LENGTH(uint8_t, &pipeline->usb->sendQueue);
+        receiveQueueLength[USB] = QUEUE_LENGTH(uint8_t, &pipeline->usb->receiveQueue);
     }
 
     if(uart::connected(pipeline->uart)) {
@@ -54,6 +58,8 @@ void openxc::pipeline::sendMessage(Pipeline* pipeline, uint8_t* message,
             ++sentMessages[UART];
             dataSent[UART] += messageSize;
         }
+        sendQueueLength[UART] = QUEUE_LENGTH(uint8_t, &pipeline->uart->sendQueue);
+        receiveQueueLength[UART] = QUEUE_LENGTH(uint8_t, &pipeline->uart->receiveQueue);
     }
 
     if(pipeline->network != NULL) {
@@ -64,6 +70,8 @@ void openxc::pipeline::sendMessage(Pipeline* pipeline, uint8_t* message,
             ++sentMessages[NETWORK];
             dataSent[NETWORK] += messageSize;
         }
+        sendQueueLength[NETWORK] = QUEUE_LENGTH(uint8_t, &pipeline->network->sendQueue);
+        receiveQueueLength[NETWORK] = QUEUE_LENGTH(uint8_t, &pipeline->network->receiveQueue);
     }
 }
 
@@ -86,12 +94,16 @@ void openxc::pipeline::logStatistics(Pipeline* pipeline) {
     static DeltaStatistic droppedMessageStats[PIPELINE_ENDPOINT_COUNT];
     static DeltaStatistic sentMessageStats[PIPELINE_ENDPOINT_COUNT];
     static DeltaStatistic dataSentStats[PIPELINE_ENDPOINT_COUNT];
+    static DeltaStatistic sendQueueStats[PIPELINE_ENDPOINT_COUNT];
+    static DeltaStatistic receiveQueueStats[PIPELINE_ENDPOINT_COUNT];
     static bool initializedStats = false;
     if(!initializedStats) {
         for(int i = 0; i < PIPELINE_ENDPOINT_COUNT; i++) {
             statistics::initialize(&droppedMessageStats[i]);
             statistics::initialize(&sentMessageStats[i]);
             statistics::initialize(&dataSentStats[i]);
+            statistics::initialize(&sendQueueStats[i]);
+            statistics::initialize(&receiveQueueStats[i]);
         }
         initializedStats = true;
     }
@@ -102,16 +114,37 @@ void openxc::pipeline::logStatistics(Pipeline* pipeline) {
             statistics::update(&sentMessageStats[i], sentMessages[i]);
             statistics::update(&droppedMessageStats[i], droppedMessages[i]);
             statistics::update(&dataSentStats[i], dataSent[i]);
+            statistics::update(&dataSentStats[i], dataSent[i]);
+
+            statistics::update(&sendQueueStats[i], sendQueueLength[i]);
+            statistics::update(&receiveQueueStats[i], receiveQueueLength[i]);
+
+            debug("Average %s receive queue fill percent: %f",
+                    interfaceName,
+                    statistics::exponentialMovingAverage(&receiveQueueStats[i])
+                        / QUEUE_MAX_LENGTH(uint8_t) * 100);
+            debug("%s receive queue max fill percent: %f",
+                    interfaceName,
+                    (float) statistics::maximum(&receiveQueueStats[i]) /
+                            QUEUE_MAX_LENGTH(uint8_t) * 100);
+            debug("Average %s send queue fill percent: %f",
+                    interfaceName,
+                    statistics::exponentialMovingAverage(&sendQueueStats[i])
+                        / QUEUE_MAX_LENGTH(uint8_t) * 100);
+            debug("%s send queue max fill percent: %f",
+                    interfaceName,
+                    (float) statistics::maximum(&sendQueueStats[i]) /
+                            QUEUE_MAX_LENGTH(uint8_t) * 100);
 
             debug("%s messages sent: %d", interfaceName,
                     sentMessageStats[i].total);
             debug("%s messages dropped: %d", interfaceName,
                     droppedMessageStats[i].total);
             if(droppedMessages[i] > 0) {
-                debug("%s message drop ratio: %f", interfaceName,
+                debug("%s message drop percent: %f", interfaceName,
                         (float)droppedMessageStats[i].total /
                             (sentMessageStats[i].total +
-                            droppedMessageStats[i].total));
+                            droppedMessageStats[i].total) * 100);
             }
             debug("Average %s sent message rate: %f msgs / s",
                     interfaceName,

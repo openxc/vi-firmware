356d4864105ed7258fa137c12253eac2b7cc705d
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-12-03 14:16:31 -0500

    Install ISR on PIC32 with updated build script.

--- src/platform/pic32/platform.cpp
+++ src/platform/pic32/platform.cpp
@@ -1,6 +1,11 @@
 #include "platform/platform.h"
 #include "WProgram.h"
 
+extern "C" {
+extern void __use_isr_install(void);
+__attribute__((section(".comment"))) void (*__use_force_isr_install)(void) = &__use_isr_install;
+}
+
 void openxc::platform::initialize() {
     // this is required to initialize the Wiring library from MPIDE's toolchain
     // (inspired by Arduino)

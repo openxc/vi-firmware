d267339fc834675f3463792fa48492dd850a7a2e
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-12-13 17:17:48 -0500

    Use correct endpoint number in USB descriptor.

--- src/platform/lpc17xx/log.cpp
+++ src/platform/lpc17xx/log.cpp
@@ -23,7 +23,7 @@ void openxc::util::log::debugNoNewline(const char* format, ...) {
 
     _printf(buffer);
 
-    // TODO
+    // TODO careful these don't log, otherwise we could loop foreeever
     conditionalEnqueue(
             &USB_DEVICE.endpoints[LOG_ENDPOINT_NUMBER - 1].sendQueue,
             (uint8_t*) buffer, strnlen(buffer, MAX_LOG_LINE_LENGTH));

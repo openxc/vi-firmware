23320e422ff562093f4a96d91d1f139befc54a9c
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-01-07 12:08:10 -0500

    Supress another error when compiling for ARM.

--- src/platform/lpc17xx/lpc17xx.mk
+++ src/platform/lpc17xx/lpc17xx.mk
@@ -23,9 +23,11 @@ CC = $(GCC_BIN)arm-none-eabi-gcc
 CPP = $(GCC_BIN)arm-none-eabi-g++
 AS_FLAGS = -c -mcpu=cortex-m3 -mthumb --defsym RAM_MODE=0
 ONLY_C_FLAGS += -std=gnu99
+SUPRESSED_ERRORS = -Wno-aggressive-loop-optimizations -Wno-char-subscripts \
+				   -Wno-unused-but-set-variable
 CC_FLAGS = -c -fno-common -fmessage-length=0 -Wall -fno-exceptions \
 		   -mcpu=cortex-m3 -mthumb -ffunction-sections -fdata-sections \
-		   -Wno-char-subscripts -Wno-unused-but-set-variable -Werror
+		   -Werror $(SUPRESSED_ERRORS)
 CC_SYMBOLS += -DTOOLCHAIN_GCC_ARM -DUSB_DEVICE_ONLY -D__LPC17XX__ -DBOARD=9
 
 ifeq ($(PLATFORM), BLUEBOARD)

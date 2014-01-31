2eca932b723ad180bae67c2146cf9d5836981186
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-01-30 19:04:36 -0500

    Split bootstrap up so you don't have to install *everything*.

---
+++ script/bootstrap/common.sh
@@ -0,0 +1,203 @@
+#!/usr/bin/env bash
+
+set -e
+BOOTSTRAP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -z $COMMON_SOURCED ]; then
+
+    # TODO this is kind of a hacky way of determining if root is required -
+    # ideally we wouuld set up a little virtualenv in the dependencies folder
+    SUDO_CMD=
+    if command -v sudo >/dev/null 2>&1; then
+        SUDO_CMD="sudo -E"
+
+        echo "The bootstrap script needs to install a few packages to your system as an admin, and we will use the 'sudo' command - enter your password to continue"
+        $SUDO_CMD ls > /dev/null
+    fi
+
+    KERNEL=`uname`
+    if [ ${KERNEL:0:7} == "MINGW32" ]; then
+        OS="windows"
+    elif [ ${KERNEL:0:6} == "CYGWIN" ]; then
+        OS="cygwin"
+    elif [ $KERNEL == "Darwin" ]; then
+        OS="mac"
+    else
+        OS="linux"
+        if ! command -v lsb_release >/dev/null 2>&1; then
+            # Arch Linux
+            if command -v pacman>/dev/null 2>&1; then
+                $SUDO_CMD pacman -S lsb-release
+            fi
+        fi
+
+        DISTRO=`lsb_release -si`
+    fi
+
+    die() {
+        echo >&2 "${bldred}$@${txtrst}"
+        exit 1
+    }
+
+    _cygwin_error() {
+        echo
+        echo "${bldred}Missing \"$1\"${txtrst} - run the Cygwin installer again and select the base package set:"
+        echo "    $CYGWIN_PACKAGES"
+        echo "After installing the packages, re-run this bootstrap script."
+        die
+    }
+
+    if ! command -v tput >/dev/null 2>&1; then
+        if [ $OS == "cygwin" ]; then
+            echo "OPTIONAL: Install the \"ncurses\" package in Cygwin to get colored shell output"
+        fi
+    else
+        txtrst=$(tput sgr0) # reset
+        bldred=${txtbld}$(tput setaf 1)
+        bldgreen=${txtbld}$(tput setaf 2)
+    fi
+
+
+    _pushd() {
+        pushd $1 > /dev/null
+    }
+
+    _popd() {
+        popd > /dev/null
+    }
+
+    _wait() {
+        if [ -z $CI ]; then
+            echo "Press Enter when done"
+            read
+        fi
+    }
+
+    _install() {
+        if [ $OS == "cygwin" ]; then
+            _cygwin_error $1
+        elif [ $OS == "mac" ]; then
+            # brew exists with 1 if it's already installed
+            set +e
+            brew install $1
+            set -e
+        else
+            if [ -z $DISTRO ]; then
+                echo
+                echo "Missing $1 - install it using your distro's package manager or build from source"
+                _wait
+            else
+                if [ $DISTRO == "arch" ]; then
+                    $SUDO_CMD pacman -S $1
+                elif [ $DISTRO == "Ubuntu" ]; then
+                    $SUDO_CMD apt-get update -qq
+                    $SUDO_CMD apt-get install $1 -y
+                else
+                    echo
+                    echo "Missing $1 - install it using your distro's package manager or build from source"
+                    _wait
+                fi
+            fi
+        fi
+    }
+
+    CYGWIN_PACKAGES="make, gcc-core, patch, unzip, python, check, curl, libsasl2, python-setuptools, ncurses"
+
+    download() {
+        url=$1
+        filename=$2
+        curl $url -L -o $filename
+    }
+
+    if [ `id -u` == 0 ]; then
+        die "Error: running as root - don't use 'sudo' with this script"
+    fi
+
+    if ! command -v curl >/dev/null 2>&1; then
+        if [ $OS == "cygwin" ]; then
+            _cygwin_error "curl"
+        else
+            _install curl
+        fi
+    fi
+
+    echo "Storing all downloaded dependencies in the \"dependencies\" folder"
+
+    DEPENDENCIES_FOLDER="dependencies"
+    mkdir -p $DEPENDENCIES_FOLDER
+
+
+    if [ $OS == "windows" ]; then
+        die "Sorry, the bootstrap script for compiling from source doesn't support the Windows console - try Cygwin."
+    fi
+
+    if [ $OS == "mac" ] && ! command -v brew >/dev/null 2>&1; then
+        echo "Installing Homebrew..."
+        ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+    fi
+
+    if ! command -v make >/dev/null 2>&1; then
+        if [ $OS == "cygwin" ]; then
+            _cygwin_error "make"
+        elif [ $OS == "mac" ]; then
+                die "Missing 'make' - install the Xcode CLI tools"
+        else
+            if [ $DISTRO == "arch" ]; then
+                $SUDO_CMD pacman -S base-devel
+            elif [ $DISTRO == "Ubuntu" ]; then
+                $SUDO_CMD apt-get update -qq
+                $SUDO_CMD apt-get install build-essential -y
+            fi
+        fi
+    fi
+
+    if ! command -v git >/dev/null 2>&1; then
+        if [ $OS == "cygwin" ]; then
+            die "You need to install Git for Windows from http://git-scm.com/ and select the option to \"Run Git from the Windows Command Prompt\" during the installation."
+        elif [ $OS == "mac" ] || [ $OS == "linux" ]; then
+            _install git
+        fi
+    fi
+
+    echo "Updating Git submodules..."
+
+    # git submodule update is a shell script and expects some lines to fail
+    set +e
+    git submodule sync
+    if ! git submodule update --init --recursive --quiet; then
+        echo "Unable to update git submodules - try running \"git submodule update --init --recursive\" to see the full error"
+        echo "If git complains that it \"Needed a single revision\", run \"rm -rf src/libs\" and then try the bootstrap script again"
+        if [ $OS == "cygwin" ]; then
+            echo "In Cygwin this may be true (ignore if you know ca-certifications is installed:"
+            _cygwin_error "ca-certificates"
+        fi
+        die
+    fi
+    set -e
+
+    if ! command -v python >/dev/null 2>&1; then
+        echo "Installing Python..."
+        _install "python"
+    fi
+
+    if ! command -v pip >/dev/null 2>&1; then
+        echo "Installing Pip..."
+        if ! command -v easy_install >/dev/null 2>&1; then
+            die "easy_install not available, can't install pip"
+        fi
+
+        $SUDO_CMD easy_install pip
+    fi
+
+    PIP_SUDO_CMD=
+    if [ -z $VIRTUAL_ENV ]; then
+        # Only use sudo if the user doesn't have an active virtualenv
+        PIP_SUDO_CMD=$SUDO_CMD
+    fi
+
+    $PIP_SUDO_CMD pip install --src dependencies --pre -Ur $BOOTSTRAP_DIR/pip-requirements.txt
+
+    popd
+
+    COMMON_SOURCED=1
+fi
---
+++ script/bootstrap/devel.sh
@@ -0,0 +1,56 @@
+set -e
+BOOTSTRAP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $BOOTSTRAP_DIR/common.sh
+
+echo "Installing dependencies for running test suite..."
+
+if [ -z $CI ] && ! command -v lcov >/dev/null 2>&1; then
+    if [ $OS == "cygwin" ]; then
+        echo "Missing lcov - Cygwin doesn't have a packaged version of lcov, and it's only required to calculate test suite coverage. We'll skip it."
+    elif [ $OS == "mac" ]; then
+        brew install lcov
+    else
+        if [ $DISTRO == "arch" ]; then
+            echo "Missing lcov - install from the AUR."
+            _wait
+        elif [ $DISTRO == "Ubuntu" ]; then
+            $SUDO_CMD apt-get update -qq
+            $SUDO_CMD apt-get install lcov -y
+        fi
+    fi
+fi
+
+if [ $OS == "mac" ]; then
+    _pushd $DEPENDENCIES_FOLDER
+    LLVM_BASENAME=clang+llvm-3.2-x86_64-apple-darwin11
+    LLVM_FILE=$LLVM_BASENAME.tar.gz
+    LLVM_URL=http://llvm.org/releases/3.2/$LLVM_FILE
+
+    if ! test -e $LLVM_FILE
+    then
+        echo "Downloading LLVM 3.2..."
+        download $LLVM_URL $LLVM_FILE
+    fi
+
+    if ! test -d $LLVM_BASENAME
+    then
+        echo "Installing LLVM 3.2 to local folder..."
+        tar -xzf $LLVM_FILE
+        echo "LLVM 3.2 installed"
+    fi
+
+    _popd
+fi
+
+if [ $OS == "cygwin" ] && ! command -v ld >/dev/null 2>&1; then
+    _cygwin_error "gcc-core"
+fi
+
+if ! ld -lcheck -o /tmp/checkcheck 2>/dev/null; then
+    echo "Installing the check unit testing library..."
+
+    _install "check"
+fi
+
+echo
+echo "${bldgreen}Core developer dependencies installed.$txtrst"

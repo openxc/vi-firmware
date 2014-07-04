3cd89ebd626a2843dd6cafc9559247f02974391d
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-07-04 13:01:57 -0400

    Compile binaries with DEBUG=0.

--- fabfile.py
+++ fabfile.py
@@ -90,6 +90,7 @@ def compile_obd2(target_path):
         local("make clean", capture=True)
         for board in env.boards:
             output = local("PLATFORM=%s " % (board['name']) +
+                    "DEBUG=0 "
                     "BOOTLOADER=1 "
                     "DEFAULT_RECURRING_OBD2_REQUESTS_STATUS=1 "
                     "DEFAULT_POWER_MANAGEMENT=OBD2_IGNITION_CHECK make -j4",
@@ -107,7 +108,7 @@ def compile_emulator(target_path):
         local("make clean", capture=True)
         for board in env.boards:
             output = local("PLATFORM=%s " % (board['name']) +
-                    "DEBUG=1 "
+                    "DEBUG=0 "
                     "BOOTLOADER=1 "
                     "DEFAULT_EMULATED_DATA_STATUS=1 "
                     "DEFAULT_POWER_MANAGEMENT=ALWAYS_ON make -j4",

d13e8a5d41fbdbcdc7cebd475b790764be945060
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-09-23 22:54:05 -0400

    Fix a double-dereference that caused protobuf deserialization to crash.
    Still doesn't work though, but now we get "Protobuf decoding failed
    with invalid wire_type".

--- src/payload/protobuf.cpp
+++ src/payload/protobuf.cpp
@@ -8,7 +8,7 @@ using openxc::util::log::debug;
 bool openxc::payload::protobuf::deserialize(uint8_t payload[], size_t length,
         openxc_VehicleMessage* message) {
     pb_istream_t stream = pb_istream_from_buffer(payload, length);
-    bool status = pb_decode(&stream, openxc_VehicleMessage_fields, &message);
+    bool status = pb_decode(&stream, openxc_VehicleMessage_fields, message);
     if(!status) {
         debug("Protobuf decoding failed with %s", PB_GET_ERROR(&stream));
     }

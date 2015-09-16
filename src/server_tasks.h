ceaae28b67ee521c6b0d8c04a7d8f989ff2598bb
Christopher Peplin <chris.peplin@rhubarbtech.com>
2015-09-16 12:57:27 -0400

    Fix case of an include to support crossplatform compilation.

--- src/server_tasks.h
+++ src/server_tasks.h
@@ -1,11 +1,11 @@
-#include "telit_HE910.h"
+#include "telit_he910.h"
 
 namespace openxc {
 namespace server_task {
-	
+
 void openxc::server_task::firmwareCheck(TelitDevice* device);
 void openxc::server_task::flushDataBuffer(TelitDevice* device);
 void openxc::server_task::commandCheck(TelitDevice* device);
-	
+
 }
 }

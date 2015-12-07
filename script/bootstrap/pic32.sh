d4ecad69b8692a57d5307c10d7a19abc7eca0a30
dsouzahansenfrancis <hdsouza@crosschasm.com>
2015-12-07 01:22:12 -0500

    Updated environment setup scripts

--- script/bootstrap/pic32.sh
+++ script/bootstrap/pic32.sh
@@ -6,6 +6,11 @@ echo "Installing dependencies for building for chipKIT Max32 platform"
 
 source $BOOTSTRAP_DIR/flashing_chipkit.sh
 
+if !  command -v p7zip >/dev/null 2>&1; then
+	echo "Installing 7zip..."
+	_install "p7zip-full"
+fi
+
 ## chipKIT libraries for USB, CAN and Network
 
 CHIPKIT_LIBRARY_AGREEMENT_URL="http://www.digilentinc.com/Agreement.cfm?DocID=DSD-0000318"
@@ -24,14 +29,44 @@ then
     download $CHIPKIT_LIBRARY_DOWNLOAD_URL $CHIPKIT_ZIP_FILE
     unzip $CHIPKIT_ZIP_FILE
 fi
+
+MLA_LIBRARY_AGREEMENT_URL="http://ww1.microchip.com/downloads/en/DeviceDoc/Microchip%20Application%20Solutions%20Users%20Agreement.pdf"
+MLA_LIBRARY_URL="http://ww1.microchip.com/downloads/en/DeviceDoc/MCHP_App_Lib_v2010_10_19_Installer.zip"
+MLA_ZIP_FILE="MCHP_App_Lib_v2010_10_19_Installer.zip"
+MLA_FOLDER_OUTPUT="MLA"
+if ! test -e $MLA_ZIP_FILE 
+then
+    echo
+    if [ -z $CI ] && [ -z $VAGRANT ]; then
+        echo "By running this command, you agree to Microchip's licensing agreement at $MLA_LIBRARY_AGREEMENT_URL"
+        echo "Press Enter to verify you have read the license agreement."
+        read
+    fi
+    download $MLA_LIBRARY_URL $MLA_ZIP_FILE
+    unzip $MLA_ZIP_FILE
+	echo "Extracting MLA"
+	7z x 'Microchip Application Libraries v2010-10-19 Installer.exe' -o$MLA_FOLDER_OUTPUT
+fi
 _popd
+
 
 _pushd src/libs
 for LIBRARY in chipKITUSBDevice chipKITCAN chipKITEthernet; do
     echo "Installing chipKIT library $LIBRARY..."
     cp -R ../../dependencies/libraries/$LIBRARY .
 done
+
+if ! test -e $MLA_FOLDER_OUTPUT
+echo "Installing MLA MSD"
+mkdir $MLA_FOLDER_OUTPUT
+mkdir $MLA_FOLDER_OUTPUT/MDD_File_System
+cp -R '../../dependencies/MLA/Microchip/USB/MSD Device Driver/' ./MLA/MSD_Device_Driver
+cp -R '../../dependencies/MLA/Microchip/MDD File System/' ./MLA/MDD_File_System
+cp -R '../../dependencies/MLA/Microchip/Include/' ./MLA/Include
+fi
+
 _popd
+
 
 ### Patch libraries to avoid problems in case sensitive operating systems
 ### See https://github.com/chipKIT32/chipKIT32-MAX/issues/146
@@ -57,6 +92,15 @@ _popd
 
 _popd
 set -e
+
+echo "Patching FSIO.C file to add flushing operation"
+set +e
+_pushd src/libs/MLA/MDD_File_System
+patch -p1 -sNi ../../../../script/FSIO-flush.patch > /dev/null
+patch -p1 -sNi ../../../../script/SD-SPI-platform.patch > /dev/null
+_popd
+set -e
+
 
 ## Python pyserial module for the reset script in Arduino-Makefile
 

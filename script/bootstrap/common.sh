8ed5af32590eb8b414ecb0c09edf15dff3181754
Christopher Peplin <chris.peplin@rhubarbtech.com>
2016-01-21 16:40:38 -0500

    Don't upgrade setuptools anymore, use current version.

--- script/bootstrap/common.sh
+++ script/bootstrap/common.sh
@@ -231,12 +231,12 @@ pre-configured Vagrant environment. See the docs for more information."
         echo "Installing Python..."
         _install "python"
     fi
-    
+
     if [ $OS != "cygwin" ]; then
         echo "Installing Python development headers..."
         _install "python-dev"
     fi
-    
+
     if ! command -v pip >/dev/null 2>&1; then
         echo "Installing Pip..."
         if ! command -v easy_install >/dev/null 2>&1; then
@@ -256,7 +256,6 @@ pre-configured Vagrant environment. See the docs for more information."
         PIP_SUDO_CMD=$SUDO_CMD
     fi
 
-    $PIP_SUDO_CMD pip install --upgrade setuptools
     $PIP_SUDO_CMD pip install --src dependencies --pre -Ur $BOOTSTRAP_DIR/ci-requirements.txt
     if [ -z $CI ]; then
         $PIP_SUDO_CMD pip install --src dependencies --pre -Ur $BOOTSTRAP_DIR/pip-requirements.txt

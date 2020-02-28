c0183df943473c921f524d7d0f602e2e0738f596
Prateek Prakash <59665714+PPRAKA30@users.noreply.github.com>
2020-02-28 12:33:23 -0500

    Resolve SonarQube red flag issues

--- script/make_trips.py
+++ script/make_trips.py
@@ -15,8 +15,7 @@ class DestFileCreator:
     def next_dest(self, data_file):
         newName = "Trip" + str(self.nextTrip).zfill(3) + "-" + data_file
         self.nextTrip = self.nextTrip + 1
-        print "Starting ",
-        print newName
+        print(f"Starting {newName}")
         return open(newName, "w")
 
 def get_next_file(trace_file):
@@ -39,72 +38,68 @@ def get_next_file(trace_file):
 
     return filename
 
-def compile_trip(trace_file, tripNum):
-    dataFileValid = True
+def read_trace_file(dataFileValid, destFileGen, destinationFile, trace_file, currentTraceFile):
     lastTimeStamp = 0.0
     currentTimeStamp = 0
-    destFileGen = DestFileCreator(tripNum)
     errorCount = 0
     lineCount = 0
+
+    for line in currentTraceFile:
+        try:
+            lineCount = lineCount + 1
+            timestamp, data = line.split(':', 1)
+            record = json.loads(data)
+        except ValueError:
+            sys.stderr.write("Skipping line: %s" % data)
+            print(" ")
+            errorCount = errorCount + 1
+            continue
+        
+        if lastTimeStamp is not 0.0:
+            if (float(timestamp) - lastTimeStamp) > 600.00:  # Time is in seconds
+                print(f"Found a gap of {float(timestamp) - lastTimeStamp} seconds. Creating new Trip file.")
+                destinationFile.close()
+                lastTimeStamp = 0.0
+                destinationFile = destFileGen.next_dest(trace_file)
+            elif (float(timestamp) - lastTimeStamp) > 1.00:  # Time is in seconds
+                print(f"Momentary dropout of {float(timestamp) - lastTimeStamp} seconds. Ignoring.")
+        lastTimeStamp = float(timestamp)
+        destinationFile.write(line)
+                
+    if dataFileValid is True:
+        currentTraceFile.close()
+        trace_file = get_next_file(trace_file)
+
+    percentBad = 100.0 * errorCount / lineCount
+    print(f"Parsed {lineCount} lines.")
+
+    print(f"Detected {errorCount} errors.")
+
+    print(f"{percentBad}% bad data")
+
+def compile_trip(trace_file, tripNum):
+    dataFileValid = True
+    destFileGen = DestFileCreator(tripNum)
+    
 
     destinationFile = destFileGen.next_dest(trace_file)
 
     while dataFileValid is True:
         try:
             currentTraceFile = open(trace_file, "r")
-        except IOError, e:
-            print e
+        except IOError as e:
+            print(e)
             dataFileValid = False
             destinationFile.close()
             break
         else:
-            print 'Opened %s' % trace_file
-
-            for line in currentTraceFile:
-                try:
-                    lineCount = lineCount + 1
-                    timestamp, data = line.split(':', 1)
-                    record = json.loads(data)
-                except ValueError:
-                    sys.stderr.write("Skipping line: %s" % data)
-                    print " "
-                    errorCount = errorCount + 1
-                    continue
-                
-                if lastTimeStamp is not 0.0:
-                    if (float(timestamp) - lastTimeStamp) > 600.00:  # Time is in seconds
-                        print "Found a gap of ",
-                        print (float(timestamp) - lastTimeStamp),
-                        print " seconds.  Creating new Trip file."
-                        destinationFile.close()
-                        lastTimeStamp = 0.0
-                        destinationFile = destFileGen.next_dest(trace_file)
-                    elif (float(timestamp) - lastTimeStamp) > 1.00:  # Time is in seconds
-                        print "Momentary dropout of ",
-                        print (float(timestamp) - lastTimeStamp),
-                        print " seconds.  Ignoring."
-                lastTimeStamp = float(timestamp)
-                destinationFile.write(line)
-                
-            if dataFileValid is True:
-                currentTraceFile.close()
-                trace_file = get_next_file(trace_file)
-
-    percentBad = 100.0 * errorCount / lineCount
-    print "Parsed",
-    print lineCount,
-    print "lines."
-
-    print "Detected",
-    print errorCount,
-    print "errors."
-
-    print percentBad,
-    print "% bad data."
+            print(f'Opened {trace_file}')
+            read_trace_file(dataFileValid, destFileGen, destinationFile, trace_file, currentTraceFile)
+            
 
 if __name__ == '__main__':
     if len(sys.argv) is not 3:
-        print "Must provide the path to the first trace file in a trip and the trip number."
+        print("Must provide the path to the first trace file in a trip and the trip number.")
         sys.exit(1)
     
     compile_trip(sys.argv[1], int(sys.argv[2]))
